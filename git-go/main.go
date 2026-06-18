0be886430ebd91bd98f364015f622dda99579e7c
Jake <jemoya1994@gmail.com>
2026-06-18 00:35:20 -0700

    messing with os.Args, learning more fun little go features as well

--- git-go/main.go
+++ git-go/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+// multiple inmports within go
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	fmt.Println("Git go is operational")
+// should essentially parse and get args by index
+// We would ideally need to know what flags potentially live within the function
+// So flags then parse those flags as 1,2,3 and so on.
+func main() {
+
+	// var w. inferred type on the os args
+	args := os.Args
+
+	if len(args) < 3 {
+		fmt.Println("Not enough args present")
+		return
+	}
+
+	flags := args[0]
+	message := args[1]
+
+	revMessage := fmt.Sprintf("Lookin good %s %s", flags, message)
+
+	fmt.Println(revMessage)
+
 }
