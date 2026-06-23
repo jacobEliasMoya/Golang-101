98b155d1f26a2831c0efd3f9f97c346e370253f9
jacob moya <jemoya1994@Gmail.com>
2026-06-23 01:15:18 -0700

    updating to include some helper functions, fairly similar to JS in terms of feel

--- git-go/main.go
+++ git-go/main.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+func formatMessage(message string) string {
+	return fmt.Sprintf("(fix) %s.", message)
+}
+
 // should essentially parse and get args by index
 // We would ideally need to know what flags potentially live within the function
 // So flags then parse those flags as 1,2,3 and so on.
@@ -17,7 +21,7 @@ func main() {
 	if len(args) < 3 {
 		fmt.Println("Not enough args present")
 		return
-	} 
+	}
 
 	// saving flags as inferred vars
 	flags := args[0]
